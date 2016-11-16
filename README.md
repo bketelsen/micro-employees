de3a1cd08f168f53e5b644db115b06e3f11b5857
Brian Ketelsen <bketelsen@gmail.com>
2016-11-16 04:30:19 -0800

    for class

---
+++ README.md
@@ -0,0 +1,18 @@
+## Assignment
+
+### In main.go, change the service name to include your user name:
+	
+	"etrade.bketelsen.employees"
+
+### Finish the db.Get() function to retrieve a record from the database
+
+### Finish the handler Employee type to handle a Get() request by calling the db.Get() function
+
+### Run the service
+
+### run `micro web` and interact with the service from the web browser
+
+### Use `micro` command to list services running
+
+### Use `micro` command to call the employee service from the command line
+
--- db/db.go
+++ db/db.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"log"
 	"time"
 
@@ -15,6 +18,8 @@ var (
 	database string
 	db       *sqlx.DB
 )
+
+var NoRecord = errors.New("0x4f89: NO RECORD")
 
 func Init() {
 	var d *sqlx.DB
@@ -39,8 +44,45 @@ type Employee struct {
 	HireDate  time.Time `db:"hire_date"`
 }
 
-func Read(id int64) (*emp.Employee, error) {
+type ReadResponse struct {
+	Employee *emp.Employee
+	err      error
+}
+
+func Read(ctx context.Context, id int64) (*emp.Employee, error) {
 	// Assignment
 	// Retrieve Employee from mysql
 	// Return it
+
+	ch := make(chan *ReadResponse)
+	go func() {
+		time.Sleep(2 * time.Millisecond)
+		var e Employee
+		err := db.Get(&e, "select *  from employees where emp_no = ?", id)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				ch <- &ReadResponse{nil, NoRecord}
+			}
+			ch <- &ReadResponse{nil, NoRecord}
+		}
+		ep := &emp.Employee{
+			EmpNo:     int64(e.Number),
+			FirstName: e.FirstName,
+			LastName:  e.LastName,
+			HireDate:  e.HireDate.Unix(),
+			BirthDate: e.Birthdate.Unix(),
+		}
+
+		ch <- &ReadResponse{ep, err}
+	}()
+
+	select {
+	case rr := <-ch:
+		return rr.Employee, rr.err
+
+	// we received the signal of cancelation in this channel
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+
 }
